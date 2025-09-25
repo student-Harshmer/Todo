d997244f531a00880fdf72c0e6aae0f290110130
Harsh <harshmer1290@gmail.com>
2025-09-25 14:58:41 +0530

    feat: made the flatList to show the tasks

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unstable-nested-components */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -6,10 +7,11 @@
  */
 
 import { useState } from 'react';
-import { StatusBar, StyleSheet, Text, useColorScheme } from 'react-native';
+import { FlatList, StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   SafeAreaView,
+  useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 
 type Task = {
@@ -19,7 +21,32 @@ type Task = {
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
-  const [task, setTask] = useState<Task[]>([]);
+  const [task, setTask] = useState<Task[]>([
+    { id: 1, title: "Learn Zustand" },
+    { id: 2, title: "Learn git and github" },
+    { id: 3, title: "Learn git and github" },
+    { id: 4, title: "Learn git and github" },
+    { id: 5, title: "Learn git and github" },
+    { id: 6, title: "Learn git and github" },
+    { id: 7, title: "Learn git and github" },
+    { id: 8, title: "Learn git and github" },
+    { id: 9, title: "Learn git and github" },
+    { id: 10, title: "Learn git and github" },
+    { id: 11, title: "Learn git and github" },
+  ]);
+
+  type TaskProps = {
+    item: Task;
+  }
+
+  const RenderTask = ({ item }: TaskProps) => {
+    return (
+      <View style={styles.taskContainer}>
+        <Text style={styles.idText}>Task ID: {item.id}</Text>
+        <Text style={styles.taskTitleText}>Title: {item.title}</Text>
+      </View>
+    )
+  }
 
   return (
     <SafeAreaProvider>
@@ -30,6 +57,12 @@ function App() {
 
 
         {/* Todo List Start here - Harsh */}
+        <FlatList
+          data={task}
+          renderItem={({ item }) => <RenderTask item={item} />}
+          contentContainerStyle={styles.listStyle}
+          showsVerticalScrollIndicator={false}
+        />
 
         {/* Todo List End Here here */}
       </SafeAreaView>
@@ -42,14 +75,33 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  container: {
-    flex: 1,
-  },
   titleText: {
     fontSize: 20,
     textAlign: 'center',
     fontWeight: 'bold',
-  }
+    paddingBottom: 10,
+  },
+  listStyle: {
+    gap: 7,
+    paddingHorizontal: 10,
+    flexGrow: 1,
+  },
+  taskContainer: {
+    backgroundColor: '#000',
+    borderRadius: 12,
+    padding: 10,
+  },
+  taskTitleText: {
+    marginTop: 10,
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 500,
+  },
+  idText: {
+    color: '#fff',
+    fontSize: 15,
+    fontWeight: 500,
+  },
 });
 
 export default App;
